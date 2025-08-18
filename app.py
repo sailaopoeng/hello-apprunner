5480bfdfb1f569612277aa3b5d253cab3e587203
Sai Lao Poeng <sailaopoeng@gmail.com>
2025-08-18 05:22:40 +0000

    change to port 8000

--- app.py
+++ app.py
@@ -36,5 +36,5 @@ async def root():
 if __name__ == "__main__":
     import uvicorn
     import os
-    port = int(os.getenv("PORT", 8080))
+    port = int(os.getenv("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
